c7c486b370d72106d51bb45d623630cb7d228005
unknown <salman.alamin01@gmail.com>
2022-11-22 22:02:37 -0800

    added thing

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,8 +7,7 @@ export default function Home() {
       <a><img class="img" src={myPic}/></a>
 
       <p>
-      hi my name is salman and i am trying to pe a programmer. before i took this calss i was a machanic at a shop 
-      and i loved to try things out. i learned alot of things that will be usful in the future
+      Hey my name is salman this is my portfolio and i will be talking about myself. I am a mechanic that is trying to become a programmer. The reason I wanted to program was because it interested me and looked fun. So I took the fullstack class and it was amazing and fun. I learned  front end and back end js and css. I feel that after finishing this class I will be ready for a programming job.
       </p>
     </div>
   );
